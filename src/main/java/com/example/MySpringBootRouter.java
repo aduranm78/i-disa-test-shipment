48d488b4088743388e81f1ea91756880a3b08324
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2022-03-03 16:18:44 -0600

    Actualiza id script

--- src/main/java/com/example/MySpringBootRouter.java
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -10,6 +10,7 @@ import java.net.URLEncoder;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.Message;
 
 /**
  * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
@@ -25,7 +26,7 @@ public class MySpringBootRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
     	
-    	String erpUri = "https://5298967-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=626&deploy=1";
+    	String erpUri = "https://5298967-sb1.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=583&deploy=1";
     	
     	onException(HttpOperationFailedException.class)
     		.handled(true)
@@ -38,6 +39,7 @@ public class MySpringBootRouter extends RouteBuilder {
     	
     	from("timer:poll?period={{timer.period}}").routeId("{{route.id}}")
     		.process(exchange -> {
+				System.out.println("******** Inicia Peticion ********:");
     			String wmsUri = env.getProperty("wms.uri");
 				System.out.println("URL WMS: " + wmsUri);
     			// String dateRange = WmsParams.getDateRange(60 * 60 * 24 * 90); // Poll interval in seconds (3 months)
@@ -68,7 +70,17 @@ public class MySpringBootRouter extends RouteBuilder {
         	.to("log:DEBUG?showBody=true&showHeaders=true")
         	.to("https://netsuite")
         	.to("log:DEBUG?showBody=true&showHeaders=true")
-        	.to("stream:out");
+			.streamCaching()
+			.process(new Processor() {
+				@Override
+				public void process(Exchange exchange) throws Exception {
+				Message message = exchange.getMessage();
+				String body = message.getBody(String.class);
+				System.out.println("Response:"+ body);
+				System.out.println("********Fin de la Peticion********");
+				}
+			});
+        	//.to("stream:out");
     }
 
 }
--- src/main/java/com/example/OAuthSign.java
+++ src/main/java/com/example/OAuthSign.java
@@ -17,10 +17,15 @@ import oauth.signpost.signature.HmacSha256MessageSigner;
 
 public class OAuthSign {
 	public static String getAuthHeader(String uri) throws IOException {           
-	    String consumer_key = "594f75ba092ebe059027f6af0a6eb3b78cdb94419282f35c1de6b91803d708aa";
+	    /*String consumer_key = "594f75ba092ebe059027f6af0a6eb3b78cdb94419282f35c1de6b91803d708aa";
 		String consumer_secret = "eba80a6a6f7374c5ba30220ca2fc7d4bf67db5120b8b327effadfe21c558de03";
 		String access_token = "1baa836e6e43d0c06aea6eb30a7233ae1974969a367092574b95f5b2a715bc98";
-		String access_secret= "e74aebe07b3827288b18776788d20b68ceae94777e4d26765884176fbabfa5b2";
+		String access_secret= "e74aebe07b3827288b18776788d20b68ceae94777e4d26765884176fbabfa5b2";*/
+
+		String consumer_key = "d2721f08ee9f3f475189103390abd251006b176b51261b09593f644f910690e4";
+		String consumer_secret = "1b12abcd83c9b1f8b0be67f0d77c697ca36464aa91258e68de0ed90c07a3e7d9";
+		String access_token = "3fb8e65762451260159843758465f8bcdfd86c2403d7729414fde83837c7c811";
+		String access_secret= "bf80771b1309cfab7fdd4d4b6ef2723d702500e4f6398b457fec13c6157679ab";
 
 	    OAuthConsumer consumer = new CommonsHttpOAuthConsumer(consumer_key, consumer_secret);
 	    consumer.setMessageSigner(new HmacSha256MessageSigner());
